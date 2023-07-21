54afc5ff1dfe7c770ab77107e6fe373008afc18d
Gabe Nataneli <gabe@sonosim.com>
2023-07-21 01:36:44 -0700

    Infinite chat loop * running the chat prompts in a loop * code clean up

--- interactive_chat.py
+++ interactive_chat.py
@@ -7,6 +7,7 @@ import fire
 
 from llama import Llama
 
+COMMAND_EXIT="exit"
 
 def main(
     ckpt_dir: str,
@@ -24,62 +25,30 @@ def main(
         max_batch_size=max_batch_size,
     )
 
-#     dialogs = [
-#         [{"role": "user", "content": "what is the recipe of mayonnaise?"}],
-#         [
-#             {"role": "user", "content": "I am going to Paris, what should I see?"},
-#             {
-#                 "role": "assistant",
-#                 "content": """\
-# Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:
+    print(f"Type '{COMMAND_EXIT}' to exit.")
 
-# 1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.
-# 2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.
-# 3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.
+    while True:
+        print("Enter prompt: ", end='' )
+        prompt = input()
+        if prompt.strip() == COMMAND_EXIT:
+            break
 
-# These are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world.""",
-#             },
-#             {"role": "user", "content": "What is so great about #1?"},
-#         ],
-#         [
-#             {"role": "system", "content": "Always answer with Haiku"},
-#             {"role": "user", "content": "I am going to Paris, what should I see?"},
-#         ],
-#         [
-#             {
-#                 "role": "system",
-#                 "content": "Always answer with emojis",
-#             },
-#             {"role": "user", "content": "How to go from Beijing to NY?"},
-#         ],
-#     ]
-#     results = generator.chat_completion(
-#         dialogs,  # type: ignore
-#         max_gen_len=max_gen_len,
-#         temperature=temperature,
-#         top_p=top_p,
-#     )
+        dialogs = [[{"role": "user", "content": f"{prompt}"}]]
 
-    print("Enter prompt: ", end='' )
-    prompt = input()
-
-    dialogs = [[{"role": "user", "content": f"{prompt}"}]]
-
-    results = generator.chat_completion(
-        dialogs,  # type: ignore
-        max_gen_len=max_gen_len,
-        temperature=temperature,
-        top_p=top_p,
-    )
-
-    for dialog, result in zip(dialogs, results):
-        for msg in dialog:
-            print(f"{msg['role'].capitalize()}: {msg['content']}\n")
-        print(
-            f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+        results = generator.chat_completion(
+            dialogs,  # type: ignore
+            max_gen_len=max_gen_len,
+            temperature=temperature,
+            top_p=top_p,
         )
-        print("\n==================================\n")
 
+        for dialog, result in zip(dialogs, results):
+            for msg in dialog:
+                print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+            print(
+                f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+            )
+            print("\n==================================\n")
 
 if __name__ == "__main__":
     fire.Fire(main)
